f0f0a60dbb5d05951220b349cdc80acf3e6d3d37
fyabc <fyabc@mail.ustc.edu.cn>
2016-07-18 20:13:32 +0800

    Add some derivative tests.

--- examples/derivatives.py
+++ examples/derivatives.py
@@ -6,9 +6,41 @@ from __future__ import print_function, unicode_literals
 import numpy as np
 from theano import function
 from theano import tensor as T
+from theano import pp
+
 
 __author__ = 'fyabc'
 
 
+def derivative():
+    x = T.dscalar('x')
+    y = x ** 2
+    gy = T.grad(y, x)
+    print(pp(gy))
+
+    f = function([x], gy)
+    print(f(4))
+    print(np.allclose(f(94.2), 94.2 * 2))
+    print(pp(f.maker.fgraph.outputs[0]))
+
+
+def gradSigmoid():
+    x = T.dmatrix('x')
+    s = T.sum(1 / (1 + T.exp(-x)))
+    gs = T.grad(s, x)
+    ds = function([x], gs)
+
+    result = ds([
+        [0, 1],
+        [-1, -2]
+    ])
+    print(result)
+
+
+def main():
+    # derivative()
+    gradSigmoid()
+
+
 if __name__ == '__main__':
-    pass
+    main()
