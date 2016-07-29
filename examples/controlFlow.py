4caac208726307bbd9047641079e9dd8aa9b192b
fyabc <fyabc@mail.ustc.edu.cn>
2016-07-29 11:45:54 +0800

    Add scan tests in controlFlow.py.

--- examples/controlFlow.py
+++ examples/controlFlow.py
@@ -6,6 +6,7 @@ from __future__ import print_function, unicode_literals
 from theano import function
 from theano import tensor as T
 from theano.ifelse import ifelse
+from theano import scan
 import theano
 import numpy
 import time
@@ -40,5 +41,39 @@ def conditions():
     print('time spent evaluating one value %f sec' % (time.clock() - tic))
 
 
+def myPower():
+    # Calculate A ** k.
+
+    k = T.iscalar('k')
+    A = T.ivector('A')
+
+    # Symbolic description of the result
+    #
+    # The parameter order of fn:
+    #   the output of the prior call to fn (or the initial value, initially),
+    #   followed by all non_sequences.
+    #
+    # outputs_info is the initial value.
+    #
+    # scan returns the result and a dictionary of updates.
+    #   the result is a 3D tensor which contains the value in each step.
+    result, updates = scan(fn=lambda priorResult, A: priorResult * A,
+                           outputs_info=T.ones_like(A),
+                           non_sequences=A,
+                           n_steps=k)
+
+    # We only care about A**k, but scan has provided us with A**1 through A**k.
+    # Discard the values that we don't care about. Scan is smart enough to
+    # notice this and not waste memory saving them.
+    finalResult = result[-1]
+
+    power = function(inputs=[A, k], outputs=finalResult, updates=updates)
+
+    print(power(range(10), 2))
+    print(power(range(10), 4))
+    print(power([1, 2, 3], 10))
+
+
 if __name__ == '__main__':
-    conditions()
+    # conditions()
+    myPower()
