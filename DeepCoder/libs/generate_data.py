6a760916b4390255f9004edbf92578ea8893bb2f
fyabc <fyabc@mail.ustc.edu.cn>
2017-03-24 16:53:35 +0800

    Some small fix.

---
+++ DeepCoder/libs/generate_data.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+# -*- encoding: utf-8 -*-
+
+"""Generate input-output pair with given program."""
+
+from __future__ import print_function, unicode_literals
+
+__author__ = 'fyabc'
