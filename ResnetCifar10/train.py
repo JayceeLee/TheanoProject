0b5d376092238d8fcf5fb8ef65a8b152d2da56b0
fyabc <fyabc@mail.ustc.edu.cn>
2017-04-08 19:53:56 +0800

    Add Resnet model on CIFAR-10.

---
+++ ResnetCifar10/train.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+# -*- encoding: utf-8 -*-
+
+"""Resnet on CIFAR-10 dataset.
+
+Theano code, with user defined optimizers.
+"""
+
+from __future__ import print_function
+
+__author__ = 'fyabc'
+
+
+class ResnetModel(object):
+    pass
+
+
+def main():
+    pass
+    
+
+if __name__ == '__main__':
+    main()
